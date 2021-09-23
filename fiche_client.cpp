7b9d0561b6c2d481a5b9180ec880bee27ff7b217
requin-citron <requin_citron@protonmail.com>
2021-09-23 23:18:23 +0200

    question 1

--- fiche_client.cpp
+++ fiche_client.cpp
@@ -1,20 +1,28 @@
 #include <iostream>
 #include <cstdlib>
+#include "fiche_client.hpp"
 using namespace std;
 
-typedef struct{
-  string nom;
-  string prenom;
-  string entreprise;
-  string mail;
-  string telephone;
-  //strings photo; //inchala on fait quoi un flux d'octet ? 
-  string data_creation;
-} fiche;
 
+
+void print_fiche(fiche *in){
+  cout << in->nom <<" "<< in->prenom << " "<< in->entreprise << " " <<\
+  in->mail << " "<< in->telephone<< " " << in->date_creation << endl;
+}
+fiche create_fiche(string nom, string prenom, string entreprise, string mail, string telephone, string date_creation){
+  fiche ret;
+  ret.nom = nom;
+  ret.prenom = prenom;
+  ret.entreprise = entreprise;
+  ret.mail = mail;
+  ret.telephone = telephone;
+  ret.date_creation = date_creation;
+  return ret;
+}
 
 
 int main(int argc, char const *argv[]) {
-  /* code */
+  fiche client = create_fiche("kleman","chevalo", "la fac", "test@example.com", "0605040302", "01-01-1971");
+  print_fiche(&client);
   return 0;
 }
--- fiche_client.hpp
+++ fiche_client.hpp
@@ -1,4 +1,15 @@
 #ifndef FICHE_CLIENT_H
 #define FICHE_CLIENT_H
+typedef struct{
+  std::string nom;
+  std::string prenom;
+  std::string entreprise;
+  std::string mail;
+  std::string telephone;
+  //strings photo; //inchala on fait quoi un flux d'octet ?
+  std::string date_creation;
+} fiche;
 
+void print_fiche(fiche *);
+fiche create_fiche(std::string, std::string, std::string, std::string, std::string, std::string);
 #endif
