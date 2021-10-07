6c63f79185d6f547d3d4d0aeef07bf6f190e2c36
requin <requin_citron@protonmail.com>
2021-10-07 09:37:43 +0200

    update contact.cpp et contact.hpp

---
+++ contact.cpp
@@ -0,0 +1,64 @@
+#include "contact.hpp"
+
+
+Contact::Contact(std::string firstName, std::string lastName, std::string enterprise, std::string mail, std::string phone, std::string pathPicture, std::string date): firstName(firstName), lastName(lastName), enterprise(enterprise), mail(mail), phone(phone), pathPicture(pathPicture), date(date){}
+
+void Contact::print_debug(){
+  std::cerr << "firstName :" << this->firstName << std::endl;
+  std::cerr << "lastName :" << this->lastName << std::endl;
+  std::cerr << "enterprise :" << this->enterprise << std::endl;
+  std::cerr << "mail :" << this->mail << std::endl;
+  std::cerr << "phone :" << this->phone << std::endl;
+  std::cerr << "pathPicture :" << this->pathPicture << std::endl;
+  std::cerr << "date :" << this->date << std::endl;
+}
+
+void Contact::setFirstName(std::string fn){
+  this->firstName = fn;
+}
+void Contact::setLastName(std::string ln){
+  this->lastName = ln;
+}
+void Contact::setEnterprise(std::string et){
+  this->enterprise = et;
+}
+void Contact::setMail(std::string ml){
+  this->mail = ml;
+}
+void Contact::setPhone(std::string ph){
+  this->phone = ph;
+}
+void Contact::setPathPicture(std::string path){
+  this->pathPicture = path;
+}
+void Contact::setDate(std::string da){
+  this->date=da;
+}
+
+std::string Contact::getFirstName(){
+  return this->firstName;
+}
+std::string Contact::getLastName(){
+  return this->lastName;
+}
+std::string Contact::getEnterprise(){
+  return this->enterprise;
+}
+std::string Contact::getMail(){
+  return this->mail;
+}
+std::string Contact::getPhone(){
+  return this->phone;
+}
+std::string Contact::getPathPicture(){
+  return this->pathPicture;
+}
+std::string Contact::getDate(){
+  return this->date;
+}
+
+int main(int argc, char const *argv[]) {
+  Contact chevalo("eva", "rodrigues", "pornhub", "chevalo@chevalo.com", "01020304", "blabla", "aze");
+  chevalo.print_debug();
+  return 0;
+}
---
+++ contact.hpp
@@ -0,0 +1,73 @@
+#ifndef HPP_CONTACT
+#define HPP_CONTACT
+#include <cstdlib>
+#include <iostream>
+
+//! class Contact
+/*!
+  permet de stocker toutes les informations d'un
+  contact.
+*/
+
+class Contact {
+private:
+  //! declaration des variables privées
+  std::string firstName;
+  std::string lastName;
+  std::string enterprise;
+  std::string mail;
+  std::string phone;
+  std::string pathPicture;
+  std::string date;
+public:
+  //! declaration des fonctions publique
+  //! Constructeur
+  /*!
+    Un unique constructeur qui demande toute les informations
+    du contact. Dans les éléments non rensegnier sont géré en amont
+    \param firstName Prenom
+    \param lastName Nom
+    \param enterprise Entreprise
+    \param mail Mail
+    \param phone Phone
+    \param pathPicture Chemain du fichier photo
+    \param date Date de creation
+  */
+  Contact (std::string firstName, std::string lastName, std::string enterprise, std::string mail, std::string phone, std::string pathPicture, std::string date);
+  //! fonction de debug
+  /*!
+    la fonction affiche tout les champs dans stderr
+  */
+  void print_debug();
+  //! fonction de mofication de firstName
+  void setFirstName(std::string fn);
+  //! fonction de mofication de lastName
+  void setLastName(std::string ln);
+  //! fonction de mofication de enterprise
+  void setEnterprise(std::string et);
+  //! fonction de mofication de mail
+  void setMail(std::string ml);
+  //! fonction de mofication de phone
+  void setPhone(std::string ph);
+  //! fonction de mofication de pathPicture
+  void setPathPicture(std::string path);
+  //! fonction de mofication de date
+  void setDate(std::string da);
+
+  //! fonction renvoyant firstName
+  std::string getFirstName();
+  //! fonction renvoyant lastName
+  std::string getLastName();
+  //! fonction renvoyant enterprise
+  std::string getEnterprise();
+  //! fonction renvoyant mail
+  std::string getMail();
+  //! fonction renvoyant phone
+  std::string getPhone();
+  //! fonction renvoyant pathPicture
+  std::string getPathPicture();
+  //! fonction renvoyant date
+  std::string getDate();
+};
+
+#endif
