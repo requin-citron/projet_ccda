0e578a8fccfa5b5793d6cefcb76f6ff637d43d92
Dupré Thibault <thibault.dupre.1999@gmail.com>
2021-10-23 13:14:57 +0200

    ajout des comms dans date

--- date.hpp
+++ date.hpp
@@ -5,20 +5,36 @@
 #include <iomanip>
 #include <algorithm>
 
+//! Class qui s'occupe de la gestion des dates
+/*!
+    Elle encapsule une structure tm et fournie toutes les méthodes necessaires pour traité avec cette objet en toutes transparance et simplicité.
+*/
 class Date {
     private:
+        //! Le structure tm appartenant au C
         tm *d;
     public:
+        //! Constructeur
         Date();
+        //! Constructeur dédié à la création des todos
         Date(std::string s);
+        //! Destructeur
         ~Date();
+        //! Accesseurs de l'attribut interne à tm
         int day() const;
+        //! Accesseurs de l'attribut interne à tm
         int mon() const;
+        //! Accesseurs de l'attribut interne à tm
         int year() const;
+        //! Accesseurs de l'attribut interne à tm
         int hour() const;
+        //! Accesseurs de l'attribut interne à tm
         int min() const;
+        //! Accesseurs de l'attribut interne à tm
         int sec() const;
+        //! Affichage simplifié avec juste la date
         std::string print() const;
+        //! Affichage complet avec les heures
         std::string printAll() const;
         friend std::ostream& operator<<(std::ostream &flux, Date const& d);
         friend bool operator==(Date const& a, Date const& b);
