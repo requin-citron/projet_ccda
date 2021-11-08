a36d67b51bd26345e2876873a43d5d04fbc8c254
requin-citron <requin_citron@protonmail.com>
2021-11-08 22:19:47 +0100

    bdd in comming

--- init.sql
+++ init.sql
@@ -1,14 +1,47 @@
-CREATE TABLE IF NOT EXISTS clients (
-    email TEXT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS contacts (
+    id INTEGER PRIMARY KEY,
+    email TEXT,
     first_name TEXT NOT NULL,
     last_name TEXT NOT NULL,
     enterprise TEXT,
     phone INTEGER UNIQUE,
-    photo TEXT UNIQUE,
-    dt datetime default current_timestamp
+    photo TEXT UNIQUE
 );
 
-CREATE TABLE IF NOT EXISTS history (
-    email TEXT PRIMARY KEY,
-    dt datetime default current_timestamp
+CREATE TABLE IF NOT EXISTS interactions (
+    id_contact INTEGER NOT NULL,
+    id_interaction INTEGER NOT NULL,
+    contenue TEXT NOT NULL,
+    PRIMARY KEY (id_contact, id_interaction)
+    FOREIGN KEY (id_contact)
+      REFERENCES contacts (id)
+      ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS tags (
+    name TEXT PRIMARY KEY,
+    id_interaction TEXT NOT NULL,
+    FOREIGN KEY (id_interaction)
+      REFERENCES interactions (id_interaction)
+      ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS history_contact (
+    id_contact INTEGER PRIMARY KEY,
+    contenue TEXT NOT NULL,
+    dt datetime default current_timestamp,
+    FOREIGN KEY (id_contact)
+      REFERENCES contacts (id)
+      ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS history_interaction (
+    id_contact INTEGER,
+    id_interaction INTEGER,
+    contenue TEXT NOT NULL,
+    dt datetime default current_timestamp,
+    PRIMARY KEY (id_contact, id_interaction)
+    FOREIGN KEY (id_contact)
+      REFERENCES contacts (id)
+      ON DELETE CASCADE
 );
