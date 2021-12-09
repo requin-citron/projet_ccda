ef0b9b39555802a2111c7b972f407dbd02a8782d
Dupré Thibault <thibault.dupre.1999@gmail.com>
2021-12-09 04:57:27 +0100

    likage de la bdd + read bdd by path dynamque

--- widgetcontact.hpp
+++ widgetcontact.hpp
@@ -11,7 +11,7 @@ public:
     WidgetContact();
     void configContact(Contact* c);
 private:
-    std::string url = "/home/thibaultdupre1999/CetC++/Qt/CDAA/projet_ccda/file/";
+    std::string url = "../file/";
     Contact* currentContact = nullptr;
     QPushButton *widgetPhoto = new QPushButton();
     QLineEdit *widgetFirstName = new QLineEdit();
