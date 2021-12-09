3ec7c2fe612bea336bbb978bb264ddc79c5b63bf
Dupré Thibault <thibault.dupre.1999@gmail.com>
2021-12-09 01:37:08 +0100

    Enorme néttoyage pour interface

--- widgetcontact.hpp
+++ widgetcontact.hpp
@@ -1,11 +1,20 @@
 #ifndef WIDGETCONTACT_HPP
 #define WIDGETCONTACT_HPP
 
+#include <QtWidgets>
 
-class WidgetContact
+class WidgetContact : public QWidget
 {
 public:
     WidgetContact();
+private:
+    QPushButton *widgetPhoto = new QPushButton();
+    QLineEdit *widgetFirstName = new QLineEdit();
+    QLineEdit *widgetLastName = new QLineEdit();
+    QLineEdit *widgetEntreprise = new QLineEdit();
+    QLineEdit *widgetMail = new QLineEdit();
+    QLineEdit *widgetPhone = new QLineEdit();
+    QListWidget *widgetListInteraction = new QListWidget();
 };
 
 #endif // WIDGETCONTACT_HPP
--- widgetmain.cpp
+++ widgetmain.cpp
@@ -1,6 +1,18 @@
 #include "widgetmain.hpp"
 
-WidgetMain::WidgetMain()
-{
-
+WidgetMain::WidgetMain(ContactCatalog *cata) : QWidget(), cata(cata) {
+    widgetListContact->setStyleSheet("QListWidget {border : 2px solid black;background : lightgreen; font-size: 50px;}"
+                                     "QListWidget QScrollBar {background : lightblue;}"
+                                     "QListView::item:selected {border : 2px solid black;background : green;}");
+    for (Contact* c: *cata->getContactLst()) {
+        QListWidgetItem *tmp = new QListWidgetItem(QString::fromStdString(c->getFirstName()+" "+c->getLastName()));
+        tmp->setTextAlignment(Qt::AlignHCenter);
+        listItem.push_back(tmp);
+        widgetListContact->addItem(tmp);
+    }
+    QPushButton *widgetAddContact = new QPushButton("Ajouter un Contact",this);
+    QVBoxLayout *l1 = new QVBoxLayout;
+    l1->addWidget(widgetListContact);
+    l1->addWidget(widgetAddContact);
+    setLayout(l1);
 }
