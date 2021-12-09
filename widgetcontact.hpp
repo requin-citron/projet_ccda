51ecdb2cf844eb6657437b7f94a50943faa0d436
Dupré Thibault <thibault.dupre.1999@gmail.com>
2021-12-09 04:17:19 +0100

    Ajout de modification de photo + ajout du changmeent du lien des photos

--- widgetcontact.hpp
+++ widgetcontact.hpp
@@ -11,7 +11,7 @@ public:
     WidgetContact();
     void configContact(Contact* c);
 private:
-    std::string url = "../file/";
+    std::string url = "/home/thibaultdupre1999/CetC++/Qt/CDAA/projet_ccda/file/";
     Contact* currentContact = nullptr;
     QPushButton *widgetPhoto = new QPushButton();
     QLineEdit *widgetFirstName = new QLineEdit();
@@ -23,6 +23,8 @@ private:
 private slots:
     void saveContact();
     void deleteContact();
+    void choosePhoto();
+    void changePathPicture();
 signals:
     void refreshContact(Contact *c);
     void removeContact(Contact *c);
