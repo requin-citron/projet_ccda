006c310b4d382077726b1d79b9eb7b963c2b898a
Dupré Thibault <thibault.dupre.1999@gmail.com>
2021-12-11 12:36:52 +0100

    widgetmain

--- widgetmain.cpp
+++ widgetmain.cpp
@@ -1,16 +1,20 @@
 #include "widgetmain.hpp"
 
 WidgetMain::WidgetMain(ContactCatalog *cata) : QWidget(), cata(cata) {
+    // ajout les contacts dans la list
     for (Contact* c: *cata->getContactLst())
         newContact(c);
     // construire les composants avec les bon texte
     paintInterface();
+    // construction de l'interface
     QVBoxLayout *l1 = new QVBoxLayout;
     l1->addWidget(widgetListContact);
     l1->addWidget(widgetAddContact);
     setLayout(l1);
 
+    // ajout d'un contact
     QObject::connect(widgetAddContact, SIGNAL(clicked()),this, SLOT(createContact()));
+    // afficher un contact
     QObject::connect(widgetListContact, SIGNAL(itemDoubleClicked(QListWidgetItem*)),this, SLOT(printContact(QListWidgetItem*)));
 }
 
@@ -29,6 +33,7 @@ void WidgetMain::rechAvance(QString s) {
 }
 
 size_t WidgetMain::getIndexContact(Contact *c) {
+    // trouver l'index du contact dans la liste
     size_t t=0;
     for (Contact *tmp: *cata->getContactLst())
         if (tmp==c)
@@ -38,6 +43,7 @@ size_t WidgetMain::getIndexContact(Contact *c) {
 }
 
 Contact* WidgetMain::getContact(int index) {
+    // trouver un contact avec l'index
     for (Contact *tmp: *cata->getContactLst())
         if (index--==0)
             return tmp;
@@ -45,27 +51,32 @@ Contact* WidgetMain::getContact(int index) {
 }
 
 void WidgetMain::newContact(Contact* c) {
+    // ajout du contact dans la list
     QListWidgetItem *tmp = new QListWidgetItem(QString::fromStdString(c->getFirstName()+" "+c->getLastName()+" ("+std::to_string(c->getId())+")"));
     tmp->setTextAlignment(Qt::AlignHCenter);
     widgetListContact->addItem(tmp);
 }
 
 void WidgetMain::removeContact(Contact *c) {
+    // suppression d'un contact
     widgetListContact->takeItem(static_cast<int>(getIndexContact(c)));
     cata->eraseContact(c);
 }
 
 void WidgetMain::createContact() {
+    // ajout d'un contact
     Contact* c = cata->addContact("Jean","Dupont","superEntreprise","jeanDupont@example.com","06...","picture.png");
     newContact(c);
     emit printContact(c);
 }
 
 void WidgetMain::refreshListWidget(Contact* c) {
+    // changer le text de l'item referant le contact
     widgetListContact->item(static_cast<int>(getIndexContact(c)))->setText(QString::fromStdString(c->getFirstName()+" "+c->getLastName()+" ("+std::to_string(c->getId())+")"));
 }
 
 void WidgetMain::printContact(QListWidgetItem* i) {
+    // demander l'affichage d'un contact
     Contact *c = getContact(widgetListContact->row(i));
     if (c!=nullptr)
         emit printContact(c);
--- widgetmain.hpp
+++ widgetmain.hpp
@@ -12,20 +12,29 @@ public:
     WidgetMain(ContactCatalog *cata);
     //! pour recharger les composants avec du texte lors d'une changement de language
     void paintInterface();
+    //! changer le text de l'item referant le contact
     void refreshListWidget(Contact* c);
+    //! supprimer un contact
     void removeContact(Contact* c);
+    //! si on demande une recherche rapide
     void rechAvance(QString s);
 private:
+    //! variable pour acceder a la list des contacts
     ContactCatalog *cata;
+    //! les widgets
     QListWidget *widgetListContact = new QListWidget();
     QPushButton *widgetAddContact = new QPushButton();
+    //! accesseurs pour la liste
     size_t getIndexContact(Contact* c);
     Contact* getContact(int index);
+    //! ajout d'un contact
     void newContact(Contact* c);
 private slots:
+    //! gestion du contact
     void createContact();
     void printContact(QListWidgetItem* i);
 signals:
+    //! demander a Windeow d'afficher un contact
     void printContact(Contact* c);
 };
 
