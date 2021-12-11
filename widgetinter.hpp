253a3addb0a6ea128508eafc6a1aea4cee9868d7
Dupré Thibault <thibault.dupre.1999@gmail.com>
2021-12-11 12:26:49 +0100

    WidgetInter

--- widgetinter.hpp
+++ widgetinter.hpp
@@ -17,22 +17,32 @@ public:
     //! si on demande une recherche rapide
     void rechAvance(QString s);
 private:
+    //! l'interaction courrante
     Interaction* currentInter = nullptr;
+    //! permet de savoir si le contenue a change pour enregistrer si besoin
     bool textChange = false;
+    //! les composants
     QLabel *widgetNameContact = new QLabel();
     QTextEdit *widgetContenu = new QTextEdit();
     QListWidget *widgetListTodo = new QListWidget();
     QLineEdit *widgetTags = new QLineEdit();
     QPushButton *widgetSave = new QPushButton();
     QPushButton *widgetDel = new QPushButton();
+    //! rafraichir les todos
     void refreshTodo();
 private slots:
+    //! modifier le contenu
     void editText();
+    //! enregistrer le contenu
     void saveText();
+    //! suppriemr une interaction
     void delInter();
+    //! enregistrer les tags
     void editTag();
 signals:
+    //! quitter WidgetInter en sauvgardant
     void refreshInteraction();
+    //! quitter WidgetInter en supprimant
     void deleteInteraction();
 };
 
